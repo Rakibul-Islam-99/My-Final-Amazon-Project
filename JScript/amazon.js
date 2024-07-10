1120dbe41f3f81086958224ee1420d265b0f8723
Rakibul_Islam <rakibulislamcse99@gmail.com>
2024-07-10 19:34:11 +0600

    import product from product .js

--- JScript/amazon.js
+++ JScript/amazon.js
@@ -1,5 +1,7 @@
-import {cart as myCart} from '../data/cart.js';
-const cart=[];
+import {cart} from '../data/cart.js';
+
+import { products } from '../data/products.js';
+
 
 let productsHTML=' ';
 

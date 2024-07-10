1e8781b64414db3a29039683e58086d63a6bbdc0
Rakibul_Islam <rakibulislamcse99@gmail.com>
2024-07-10 16:36:29 +0600

    cadjkhg

--- JScript/amazon.js
+++ JScript/amazon.js
@@ -88,7 +88,7 @@ document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
 
     document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
 
-
+   console.log(cartQuantity);
   });
 
 });
